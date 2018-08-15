aea959a1f8ff921a6231f8bae37c374897a206a5
Luke Elliott <revlucio@gmail.com>
2018-08-15 20:10:34 +0100

    ensured homepage is index.html

---
+++ src/Web/StaticFilesExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+
+namespace StreetRunner.Web
+{
+    public static class StaticFilesExtensions
+    {
+        public static void UseStaticFiles(this IApplicationBuilder app, string fileDir, string defaultFile)
+        {
+            var options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add(defaultFile);
+            app.UseDefaultFiles(options);
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), fileDir)),
+            });
+        }
+    }
+}
